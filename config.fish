f41461e14245da106358b748ec9f7718c83cf7da
Juri Adams <juri@adams.sh>
2022-11-22 14:13:57 +0100

    Delete config.fish

--- config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-# remove greeting message
-set fish_greeting
-
-# init `starship`
-starship init fish | source
